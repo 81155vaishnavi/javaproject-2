6b53f68e2007956914f562da03a7cf94e8dee924
81155vaishnavi <wakdevaishnavi2002@gmail.com>
2024-09-22 21:22:11 +0530

    by VAISHU2

---
+++ demo4/src/com/cjc/Test.java
@@ -0,0 +1,15 @@
+package com.cjc;
+
+public class Test {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("main method");
+	}
+	
+	public void m1()
+	{
+		System.out.println("m1 method  added");
+	}
+
+}
